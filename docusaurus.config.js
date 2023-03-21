eb183c7dbb6f48c94290aee8dd65354c2c44e652
onmyoji <onmyoji@qq.com>
2023-03-21 16:06:31 +0800

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,96 @@
+/*
+ * @Author: onmyoji onmyoji@qq.com
+ * @Date: 2023-03-21 14:28:02
+ * @LastEditors: onmyoji onmyoji@qq.com
+ * @LastEditTime: 2023-03-21 15:54:26
+ * @FilePath: \nahida-cn\docusaurus.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '世界第一可爱',
+  tagline: '网站还在建设中',
+  favicon: 'http://img.up.cdn.nahida.cn/nahida/icon.jpg',
+
+  // Set the production url of your site here
+  url: 'https://www.nahida.cn',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  // organizationName: 'facebook', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        // docs: {
+        //   sidebarPath: require.resolve('./sidebars.js'),
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: '世界第一可爱',
+        logo: {
+          alt: '纳西妲',
+          src: 'http://img.up.cdn.nahida.cn/nahida/icon.jpg',
+        },
+        
+      },
+      footer: {
+        style: 'dark',
+
+        copyright: `<p>Copyright  ©世界第一可爱  ${new Date().getFullYear()}.</p>
+        <a href="https://beian.miit.gov.cn/" target="_blank" style="color:#fff">蜀ICP备2023005596号`,
+
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/index.js
@@ -0,0 +1,48 @@
+/*
+ * @Author: onmyoji onmyoji@qq.com
+ * @Date: 2023-03-21 14:28:02
+ * @LastEditors: onmyoji onmyoji@qq.com
+ * @LastEditTime: 2023-03-21 16:02:26
+ * @FilePath: \nahida-cn\src\pages\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            看点其他的吧
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`欢迎来到 ${siteConfig.title}`}
+      description="纳西妲世界第一可爱 <head />">
+      <HomepageHeader />
+      <main>
+      <img src='http://img.up.cdn.nahida.cn/nahida/bg1.png' style={{width:'100%'}}></img>
+      </main>
+    </Layout>
+  );
+}
